47b9da1e0baa1d0480202ed3993f8be440e05d9d
A.J <aijony@tensorsaurus.com>
2023-10-27 14:18:51 +0200

    Overhaul deck with initial fixes
      - Clear up many cards
      - Reformat templates
      - Apply lots of corrections

--- clean.sh
+++ clean.sh
@@ -2,5 +2,5 @@
 
 # Replace the text in the last element of every field 
 # in entries with __type__ "Note" with an empty string using jq
-jq --indent 4 '.notes[] |= if (.__type__ == "Note") then .fields[-1] |= "" else . end' deck.json \
+jq --indent 4 '.children[].notes[] |= if (.__type__ == "Note") then .fields[-1] = "" else . end' deck.json \
 	 > deck.tmp && mv deck.tmp deck.json
