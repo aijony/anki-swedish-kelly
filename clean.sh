9a91e9947091156f1f395b74b78d5804a5500490
A.J. <aijony@tensorsaurus.com>
2024-07-09 08:50:42 -0700

    Update cards and corresponding notes
      - Fix styling issues
      - Indent card code
      - Improve grammer of card (with js)
      - Add grammatical hints (with css)
      - Add links to dictionaries
      - Add a frequency field
      - Add a postfix field
      - Various manual fixes to notes

--- clean.sh
+++ clean.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Replace the text in the last element of every field 
+# Replace the text in the second to last element which is notes
 # in entries with __type__ "Note" with an empty string using jq
-jq --indent 4 '.children[].notes[] |= if (.__type__ == "Note") then .fields[-1] = "" else . end' deck.json \
+jq --indent 4 '.children[].notes[] |= if (.__type__ == "Note") then .fields[-2] = "" else . end' deck.json \
 	 > deck.tmp && mv deck.tmp deck.json
 
 awk '!/^.*"leech"?.$/' deck.json > deck.tmp && mv deck.tmp deck.json
