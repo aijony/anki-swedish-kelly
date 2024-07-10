92c9fce7eb6a088aa66ab8f2fa05c567677d541e
A.J. <aijony@tensorsaurus.com>
2024-07-10 12:16:47 -0700

    Add note to reveal details

--- clean.sh
+++ clean.sh
@@ -2,7 +2,7 @@
 
 # Replace the text in the second to last element which is notes
 # in entries with __type__ "Note" with an empty string using jq
-jq --indent 4 '.children[].notes[] |= if (.__type__ == "Note") then .fields[-2] = "" else . end' deck.json \
-	 > deck.tmp && mv deck.tmp deck.json
+jq --indent 4 '.children[].notes[] |= if (.__type__ == "Note") then .fields[-1] = "" else . end' deck.json \
+   > deck.tmp && mv deck.tmp deck.json
 
 awk '!/^.*"leech"?.$/' deck.json > deck.tmp && mv deck.tmp deck.json
